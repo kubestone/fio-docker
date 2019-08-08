16d66de0ec5e274c835831c51ed8c37fba8a06d6
Julio Santiesteban Giménez <juliosg@gmx.es>
2019-08-08 21:55:00 +0200

    Create a separate working directory for each run

--- run.sh
+++ run.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 set -eo pipefail
 
-# First remove everything from the workdir
-rm -rf /data/*
-
-cd /jobs
-
 # Check required parameters
 if [ -z "$JOB_FILES" ]; then
   echo 'Missing environment variable: $JOB_FILES'
   exit 1
 fi
+
+# Copy the jobs to the working directory
+WORKDIR=${WORKDIR:-`date +%s`}
+echo "Working directory: /data/$WORKDIR"
+cd /jobs
+mkdir -p /data/$WORKDIR
+cp $JOB_FILES /data/$WORKDIR
+cd /data/$WORKDIR
 
 # Download specified remote files
 if [ ! -z "$REMOTE_JOB_FILES" ]; then
@@ -19,9 +22,6 @@ if [ ! -z "$REMOTE_JOB_FILES" ]; then
     done
 fi
 
-# Copy the jobs to the working directory
-cp $JOB_FILES /data
-cd /data
 echo "Running jobs: $JOB_FILES"
 
 # Run the jobs
